30f22a2ff09e07e02415bd59347996d293cd10d6
Edwin-Rizaldi <deycapryo4@gmail.com>
2020-05-30 16:31:24 +0800

    Selesai

---
+++ assets/textlengkap.js
@@ -0,0 +1,71 @@
+var a = 0;
+
+function read1() {
+  if (!a) {
+    document.getElementById('more1').style.display = "inline";
+    document.getElementById('dots1').style.display = "none";
+    document.getElementById('read1').innerHTML = "Tutup";
+    a = 1;
+  } else {
+    document.getElementById('more1').style.display = "none";
+    document.getElementById('dots1').style.display = "inline";
+    document.getElementById('read1').innerHTML = "Baca selengkapnya";
+    a = 0;
+  }
+}
+
+function read2() {
+  if (!a) {
+    document.getElementById('more2').style.display = "inline";
+    document.getElementById('dots2').style.display = "none";
+    document.getElementById('read2').innerHTML = "Tutup";
+    a = 1;
+  } else {
+    document.getElementById('more2').style.display = "none";
+    document.getElementById('dots2').style.display = "inline";
+    document.getElementById('read2').innerHTML = "Baca selengkapnya";
+    a = 0;
+  }
+}
+
+function read3() {
+  if (!a) {
+    document.getElementById('more3').style.display = "inline";
+    document.getElementById('dots3').style.display = "none";
+    document.getElementById('read3').innerHTML = "Tutup";
+    a = 1;
+  } else {
+    document.getElementById('more3').style.display = "none";
+    document.getElementById('dots3').style.display = "inline";
+    document.getElementById('read3').innerHTML = "Baca selengkapnya";
+    a = 0;
+  }
+}
+
+function read4() {
+  if (!a) {
+    document.getElementById('more4').style.display = "inline";
+    document.getElementById('dots4').style.display = "none";
+    document.getElementById('read4').innerHTML = "Tutup";
+    a = 1;
+  } else {
+    document.getElementById('more4').style.display = "none";
+    document.getElementById('dots4').style.display = "inline";
+    document.getElementById('read4').innerHTML = "Baca selengkapnya";
+    a = 0;
+  }
+}
+
+function read5() {
+  if (!a) {
+    document.getElementById('more5').style.display = "inline";
+    document.getElementById('dots5').style.display = "none";
+    document.getElementById('read5').innerHTML = "Tutup";
+    a = 1;
+  } else {
+    document.getElementById('more5').style.display = "none";
+    document.getElementById('dots5').style.display = "inline";
+    document.getElementById('read5').innerHTML = "Baca selengkapnya";
+    a = 0;
+  }
+}
